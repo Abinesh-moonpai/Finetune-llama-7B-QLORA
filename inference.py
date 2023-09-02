a39f941471f0c203362fb619fd2d2826aab3f5ce
Abinesh-moonpai <139101471+Abinesh-moonpai@users.noreply.github.com>
2023-09-02 10:57:27 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,5 +1,4 @@
 import argparse
-
 import torch
 import yaml
 from langchain import PromptTemplate
@@ -20,13 +19,28 @@ def read_yaml_file(file_path):
         except yaml.YAMLError as e:
             print(f"Error reading YAML file: {e}")
 
-def get_prompt(human_prompt):
-    prompt_template=f"### HUMAN:\n{human_prompt}\n\n### RESPONSE:\n"
+def get_prompt(human_prompt, context):
+    # Modified the prompt template with a more descriptive guide for the model
+    prompt_template = f"""You are an SQL agent. Given the following natural language question and the SQL table context, please formulate an accurate and efficient SQL query that would correctly answer the question. Donot deviate from the. fact that the answer always has to be an SQL Query. Use all the required joins and other syntaxes.
+
+Natural Language Question: {human_prompt}
+SQL Table Context: {context}
+
+Formulate SQL Query:"""
     return prompt_template
 
-def get_llm_response(prompt):
-    raw_output = pipe(get_prompt(prompt))
-    return raw_output
+
+
+def get_llm_response(prompt, context):
+    raw_output = pipe(get_prompt(prompt, context))
+    # Retrieve the SQL query part only (this part may need further adjustment)
+    sql_query = raw_output[0]['generated_text'].split('Formulate SQL Query:')[-1].strip()
+    return sql_query
+
+# def get_llm_response(prompt, context):
+#     raw_output = pipe(get_prompt(prompt, context))
+#     return raw_output
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -49,9 +63,14 @@ if __name__ == "__main__":
         model=model, 
         tokenizer=tokenizer, 
         max_length=512,
-        temperature=0.7,
+        temperature=0.1,
         top_p=0.95,
         repetition_penalty=1.15
     )
 
-    print(get_llm_response("What is your favorite movie?"))
+    question = "What are the distinct creation years of the departments managed by a secretary born in state 'Alabama'?"
+    context = "CREATE TABLE department (creation VARCHAR, department_id VARCHAR); CREATE TABLE management (department_id VARCHAR, head_id VARCHAR); CREATE TABLE head (head_id VARCHAR, born_state VARCHAR)"
+    
+    print(get_llm_response(question, context))
+
+    
